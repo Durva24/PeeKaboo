f53e82554c0373939b5ffca760b5bb7caba10b2a
Om  Patil <omsandeeppatil02@gmail.com>
2024-08-18 14:23:03 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+const CONFIG = {
+    GOOGLE_API_KEY: "AIzaSyAyT8kQE6p5vY_b1j10d_67if83JGk9uOU",
+    GOOGLE_SEARCH_ENGINE_ID: "70c6bb9497972479d",
+    GROQ_API_KEY: "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",
+};
